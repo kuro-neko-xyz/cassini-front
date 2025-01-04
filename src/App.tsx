7ad0f71609df567b7ae7548fee50ae3b3a687de7
kuro-neko-xyz <a.aleman.h@icloud.com>
2025-01-04 01:44:45 -0600

    Created basic editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [style, setStyle] = useState(`
+    h1 {
+      color: white;
+    }
+    `)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <style>{style}</style>
+      <h1>Customize me!</h1>
+      <textarea
+        value={style}
+        onChange={(e) => setStyle(e.target.value)}
+        style={{ width: '100%', height: '200px' }}
+      />
     </>
   )
 }

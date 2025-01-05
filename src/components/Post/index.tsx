a84654dab8067ed4e725c2fae95bae2260fa09b8
kuro-neko-xyz <a.aleman.h@icloud.com>
2025-01-05 01:31:45 -0600

    Added styled-components

--- src/components/Post/index.tsx
+++ src/components/Post/index.tsx
@@ -1,14 +1,20 @@
-import PostProps from "./models";
+import styled from 'styled-components';
+import PostProps from './models';
+
+const Container = styled.div`
+  background-color: white;
+  color: black;
+`;
 
 const Post = ({ content, date, picture, user, username }: PostProps) => {
   return (
-    <div>
+    <Container>
       <img src={picture} alt={user} />
       <h1>{user}</h1>
       <h2>{username}</h2>
       <h3>{date}</h3>
       <p>{content}</p>
-    </div>
+    </Container>
   );
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
 import { BrowserRouter, Route, Routes } from 'react-router';
 import PostPage from './pages/PostPage/index.tsx';
+import FeedPage from './pages/FeedPage/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<App />} />
+        <Route path="/" element={<FeedPage />} />
         <Route path="/post" element={<PostPage />} />
       </Routes>
     </BrowserRouter>
